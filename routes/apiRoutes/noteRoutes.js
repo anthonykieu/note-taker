48696cf882ffb2729cf78c97dd5a1d2429baaa45
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-11-27 17:52:20 -0500

    testing code

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,35 +1,34 @@
 const router = require("express").Router();
-const { filterByQuery, findById, createNewAnimal, validateAnimal, } = require("../../lib/animals");
+const { filterByQuery, findById, createNewAnimal, validateAnimal, } = require("../../public/js/index");
 const { notes } = require("../../db/db");
 
-// router.get("/animals", (req, res) => {
-//   let results = animals;
-//   if (req.query) {
-//     results = filterByQuery(req.query, results);
-//   }
-//   res.json(results);
-// });
+router.get("/notes", (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
-// router.get("/animals/:id", (req, res) => {
-//   const result = findById(req.params.id, animals);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
+router.get("/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
-// router.post("/animals", (req, res) => {
-//   // set id based on what the next index of the array will be
-//   req.body.id = animals.length.toString();
+router.post("/", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
 
-//   // if any data in req.body is incorrect, send 400 error back
-//   if (!validateAnimal(req.body)) {
-//     res.status(400).send("The animal is not properly formatted.");
-//   } else {
-//     const animal = createNewAnimal(req.body, animals);
-//     res.json(animal);
-//   }
-// });
-
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const notes = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 module.exports = router;
