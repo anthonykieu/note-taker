6410fc7685ad246e43feae6f30a5fececa9428b6
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-11-28 15:00:11 -0500

    fixed css

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const { filterByQuery, findById, createNewNote, validateNote, } = require("../../lib/notes");
-const {notes} = require("../../db/db");
+const { filterByQuery, findById, createNewNote, validateNote } = require("../../lib/notes");
+const { notes } = require("../../db/db");
 const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
     let results = notes;
@@ -30,4 +32,36 @@ router.post("/notes", (req, res) => {
     res.json(newNote);
 
 });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//     let deleted = req.params.id;
+//     let results = notes;
+//     const deletedNote = deleteNote(deleted, results);
+//     res.json(deletedNote);
+
+// });
+
+
+//Delete Note
+router.delete("/notes/:id", (req, res) => {
+    let deletedNote = req.params.id;
+    console.log(deletedNote);
+
+    fs.readFile("db/db.json", (err, notes) => {
+        if (err) throw err;
+        notesArray = JSON.parse(notes);
+        console.log(notesArray);
+        for (let i = 0; i < notesArray.length; i++) {
+            if (deletedNote === notesArray[i].id) {
+                res.json(notesArray.splice(i, 1));
+                // console.log(notesArray);
+            }
+        }
+        fs.writeFileSync("db/db.json", JSON.stringify(notesArray, null, 2), err => {
+            if (err) throw err;
+            console.log(`Deleted Note #${deletedNote}`)
+        });
+    });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const htmlRoutes = require('./routes/htmlRoutes/index');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
+app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
